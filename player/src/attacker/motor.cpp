21258529d5d58704dffa461fa28382680a24649d
ctpena <git@ctpena.net>
2023-09-16 08:57:27 +0900

    Add Motor class

--- player/src/attacker/motor.cpp
+++ player/src/attacker/motor.cpp
@@ -1,37 +1,16 @@
 #include "motor.h"
 
-// Motor A connections
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
-// Motor B connections
-int enB = 3;
-int in3 = 5;
-int in4 = 4;
+Motor motorA(9, 8, 7);
+Motor motorB(3, 5, 4);
 
 void motorSetup() {
-    // Set all the motor control pins to outputs
-    pinMode(enA, OUTPUT);
-    pinMode(enB, OUTPUT);
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-
-    // Turn off motors - Initial state
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-
+    motorA.setup();
+    motorB.setup();
 }
 
 void motorLoop() {
-    analogWrite(enA, 40);
-    analogWrite(enB, 40);
-
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
+    motorA.setSpeed(0);
+    motorB.setSpeed(0);
+    motorA.setDirection(false);
+    motorB.setDirection(false);
 }
--- player/src/attacker/motor.h
+++ player/src/attacker/motor.h
@@ -3,6 +3,43 @@
 
 #include <Arduino.h>
 
+class Motor {
+private:
+    int enPin; //pwm
+    int inPin1;
+    int inPin2;
+
+public:
+    Motor(int en, int in1, int in2)
+            : enPin(en), inPin1(in1), inPin2(in2) {}
+
+    void setup() const {
+        pinMode(enPin, OUTPUT);
+        pinMode(inPin1, OUTPUT);
+        pinMode(inPin2, OUTPUT);
+        stop();
+    }
+
+    void setSpeed(int speed) const {
+        analogWrite(enPin, speed);
+    }
+
+    void setDirection(bool pn) const {
+        if (pn) {
+            digitalWrite(inPin1, HIGH);
+            digitalWrite(inPin2, LOW);
+        } else {
+            digitalWrite(inPin1, LOW);
+            digitalWrite(inPin2, HIGH);
+        }
+    }
+
+    void stop() const {
+        digitalWrite(inPin1, LOW);
+        digitalWrite(inPin2, LOW);
+    }
+};
+
 void motorSetup();
 
 void motorLoop();
